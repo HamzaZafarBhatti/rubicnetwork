3c67871dfc07fcc6b47c1b690c50c36afa3081e9
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-05-29 05:35:05 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Setting;
+// use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $data['setting'] = Setting::first();
+        // $data['setting'] = Setting::first();
 
-        View::share($data);
+        // View::share($data);
     }
 }
