6bd1a163b3c1dec5fcb8b548fd95d99931f30669
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-06-24 03:59:06 +0500

    sdv

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -372,7 +372,7 @@
                                 <li class="nav-item"><a href="{{ route('admin.currency') }}"
                                         class="nav-link"><i class="icon-coin-euro"></i>Currency</a></li>
                                 <li class="nav-item"><a href="{{ route('admin.faqs.index') }}"
-                                        class="nav-link"><i class="icon-question4"></i>new FAQs</a></li>
+                                        class="nav-link"><i class="icon-question4"></i>FAQs</a></li>
                                 <li class="nav-item"><a href="{{ route('admin.terms') }}"
                                         class="nav-link"><i class="icon-file-check"></i>Terms & Condition</a>
                                 </li>
