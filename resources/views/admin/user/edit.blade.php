41abdaafe47661c52295fdf6570a55d67e80e3af
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-07-01 03:28:10 +0500

    Update edit.blade.php

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -353,98 +353,11 @@
                 </div>
             </div>
         </div>
-        {{-- <div class="row">
+        <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header header-elements-inline">
-                        <h6 class="card-title font-weight-semibold">Deposit Logs</h6>
-                    </div>
-                    <div class="">
-                        <table class="table datatable-show-all">
-                            <thead>
-                                <tr>
-                                    <th>S/N</th>
-                                    <th>Amount</th>
-                                    <th>BTC</th>
-                                    <th>Method</th>
-                                    <th>Ref</th>
-                                    <th>Charge</th>
-                                    <th>Status</th>
-                                    <th>Created</th>
-                                    <th>Updated</th>
-                                    <th class="text-center">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($deposit as $k => $val)
-                                    <tr>
-                                        <td>{{ ++$k }}.</td>
-                                        <td>{{ number_format($val->amount) . $currency->name }}</td>
-                                        <td>{{ convertFloat($val->btc_amo) }}BTC</td>
-                                        <td>{{ $val->gateway['name'] }}</td>
-                                        <td>{{ $val->trx }}</td>
-                                        <td>{{ number_format($val->charge) . $currency->name }}</td>
-                                        <td>
-                                            @if ($val->status == 0)
-                                                <span class="badge badge-danger">Pending</span>
-                                            @elseif($val->status == 1)
-                                                <span class="badge badge-success">Approved</span>
-                                            @endif
-                                        </td>
-                                        <td>{{ date('Y/m/d h:i:A', strtotime($val->created_at)) }}</td>
-                                        <td>{{ date('Y/m/d h:i:A', strtotime($val->updated_at)) }}</td>
-                                        <td class="text-center">
-                                            <div class="list-icons">
-                                                <div class="dropdown">
-                                                    <a href="#" class="list-icons-item" data-toggle="dropdown">
-                                                        <i class="icon-menu9"></i>
-                                                    </a>
-                                                    <div class="dropdown-menu dropdown-menu-right">
-                                                        @if ($val->status == 0)
-                                                            <a class='dropdown-item'
-                                                                href="{{ url('/') }}/admin/approvedeposit/{{ $val->id }}"><i
-                                                                    class="icon-thumbs-up3 mr-2"></i>Approve request</a>
-                                                        @endif
-                                                        <a data-toggle="modal" data-target="#{{ $val->id }}delete"
-                                                            class="dropdown-item"><i
-                                                                class="icon-bin2 mr-2"></i>Delete</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <div id="{{ $val->id }}delete" class="modal fade" tabindex="-1">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close"
-                                                        data-dismiss="modal">&times;</button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <h6 class="font-weight-semibold">Are you sure you want to delete this?
-                                                    </h6>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-link"
-                                                        data-dismiss="modal">Close</button>
-                                                    <a href="{{ url('/') }}/admin/deposit/delete/{{ $val->id }}"
-                                                        class="btn bg-danger">Proceed</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-        {{-- <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header header-elements-inline">
-                        <h6 class="card-title font-weight-semibold">Withdraw logs</h6>
+                        <h6 class="card-title font-weight-semibold">Rubic Wallet Withdraw logs</h6>
                     </div>
                     <div class="">
                         <table class="table datatable-show-all">
@@ -454,7 +367,6 @@
                                     <th>Amount</th>
                                     <th>Details</th>
                                     <th>Status</th>
-                                    <th>Type</th>
                                     <th>Created</th>
                                     <th>Updated</th>
                                     <th class="text-center">Action</th>
@@ -464,8 +376,8 @@
                                 @foreach ($withdraw as $k => $val)
                                     <tr>
                                         <td>{{ ++$k }}.</td>
-                                        <td>{{ substr($val->amount, 0, 9) }}BTC</td>
-                                        <td>{{ $val->details }}</td>
+                                        <td>{{ substr($val->amount, 0, 9) }}</td>
+                                        <td>{{ $val->account_no }}</td>
                                         <td>
                                             @if ($val->status == 0)
                                                 <span class="badge badge-danger">Unpaid</span>
@@ -473,15 +385,6 @@
                                                 <span class="badge badge-success">Paid</span>
                                             @elseif($val->status == 2)
                                                 <span class="badge badge-info">Declined</span>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if ($val->type == 1)
-                                                <span class="badge badge-info">Trading profit</span>
-                                            @elseif($val->type == 2)
-                                                <span class="badge badge-info">Account balance</span>
-                                            @elseif($val->type == 3)
-                                                <span class="badge badge-info">Referral bonus</span>
                                             @endif
                                         </td>
                                         <td>{{ date('Y/m/d h:i:A', strtotime($val->created_at)) }}</td>
@@ -495,15 +398,14 @@
                                                     <div class="dropdown-menu dropdown-menu-right">
                                                         @if ($val->status == 0)
                                                             <a class='dropdown-item'
-                                                                href="{{ url('/') }}/admin/approvewithdraw/{{ $val->id }}"><i
+                                                                href="{{ route('admin.wallet.withdraw_approve', $val->id) }}"><i
                                                                     class="icon-thumbs-up3 mr-2"></i>Approve request</a>
                                                             <a class='dropdown-item'
-                                                                href="{{ url('/') }}/admin/declinewithdraw/{{ $val->id }}"><i
+                                                                href="{{ route('admin.wallet.withdraw_decline', $val->id) }}"><i
                                                                     class="icon-thumbs-down3 mr-2"></i>Decline request</a>
                                                         @endif
                                                         <a data-toggle="modal" data-target="#{{ $val->id }}delete"
-                                                            class="dropdown-item"><i
-                                                                class="icon-bin2 mr-2"></i>Delete</a>
+                                                            class="dropdown-item"><i class="icon-bin2 mr-2"></i>Delete</a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -523,8 +425,8 @@
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-link"
                                                         data-dismiss="modal">Close</button>
-                                                    <a href="{{ url('/') }}/admin/withdraw/delete/{{ $val->id }}"
-                                                        class="btn bg-danger">Proceed</a>
+                                                        <a href="{{ route('admin.wallet.withdraw_delete', $val->id) }}"
+                                                            class="btn bg-danger">Proceed</a>
                                                 </div>
                                             </div>
                                         </div>
@@ -535,7 +437,7 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
         {{-- <div class="row">
             <div class="col-md-12">
                 <div class="card">
@@ -821,6 +723,38 @@
                             </thead>
                             <tbody>
                                 @foreach ($indirect_referral as $k => $val)
+                                    <tr>
+                                        <td>{{ ++$k }}.</td>
+                                        <td>{{ $val->referral->name ?? 'N/A' }}</td>
+                                        <td>{{ $val->referral->username ?? 'N/A' }}</td>
+                                        <td>{{ date('Y/m/d', strtotime($val->created_at)) }}</td>
+                                        <td>{{ date('Y/m/d h:i:A', strtotime($val->updated_at)) }}</td>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header header-elements-inline">
+                        <h6 class="card-title font-weight-semibold">Stake Referrals</h6>
+                    </div>
+                    <div class="">
+                        <table class="table datatable-show-all">
+                            <thead>
+                                <tr>
+                                    <th>S/N</th>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Created</th>
+                                    <th>Updated</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($stake_referral as $k => $val)
                                     <tr>
                                         <td>{{ ++$k }}.</td>
                                         <td>{{ $val->referral->name ?? 'N/A' }}</td>
