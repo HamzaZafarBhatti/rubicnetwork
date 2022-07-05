02f2caa114f68c6f698791294c8a925f4e525338
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-07-06 00:11:13 +0500

    Update StakeReferralController.php

--- app/Http/Controllers/StakeReferralController.php
+++ app/Http/Controllers/StakeReferralController.php
@@ -20,7 +20,7 @@ class StakeReferralController extends Controller
 
     public function convert()
     {
-        $converts = StakeReferralConvert::with('user')->get();
+        $converts = StakeReferralConvert::with('user')->whereRefereeId(auth()->user()->id)->get();
         return view('user.stake_referrals.convert', compact('converts'));
     }
 
