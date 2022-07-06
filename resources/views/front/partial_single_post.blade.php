a569b50920a2d3af38a4c79c8054d5b81a46d869
Hamza Zafar Bhatti <49752653+HamzaZafarBhatti@users.noreply.github.com>
2022-07-06 21:38:09 +0500

    Update partial_single_post.blade.php

--- resources/views/front/partial_single_post.blade.php
+++ resources/views/front/partial_single_post.blade.php
@@ -1,4 +1,4 @@
-@if (\Carbon\Carbon::parse($post->created_at)->format('Y-m-d') == \Carbon\Carbon::now()->format('Y-m-d') && !$is_shared)
+@if (\Carbon\Carbon::parse($post->post_date)->format('Y-m-d') == \Carbon\Carbon::now()->format('Y-m-d') && !$is_shared)
     <a type="button" class="button button--effect button--last" href="{{ route('user.viral_shares.earn', $post->id) }}" id="viral_earn">
         CLICK TO EARN <i class="fas fa-share"></i>
     </a>
